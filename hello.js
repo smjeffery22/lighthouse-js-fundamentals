8192808e2580114d2090db53ecad5b423693ea7e
Jeffery <parkjon8@gmail.com>
2021-11-07 02:46:07 +0000

    sayHello function

---
+++ hello.js
@@ -0,0 +1,15 @@
+/*
+const sayHello = function () {
+  console.log("Hello, world");
+}
+
+sayHello();
+*/
+
+const sayHello = function (name) {
+  console.log("Hello, " + name);
+}
+
+sayHello("Jeffery");
+sayHello("John");
+sayHello("Dennis");
