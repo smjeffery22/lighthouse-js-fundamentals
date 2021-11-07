27f8ad66973bb4b4cafa8b7359971643ec3028c8
Jeffery <parkjon8@gmail.com>
2021-11-07 03:19:14 +0000

    added return example

--- hello.js
+++ hello.js
@@ -6,6 +6,7 @@ const sayHello = function () {
 sayHello();
 */
 
+/*
 const sayHello = function (name) {
   console.log("Hello, " + name);
 }
@@ -13,3 +14,15 @@ const sayHello = function (name) {
 sayHello("Jeffery");
 sayHello("John");
 sayHello("Dennis");
+*/
+
+const sayHelloToConsole = function (name) {
+  console.log("Hello, " + name);
+}
+sayHelloToConsole("John");
+
+const returnSayHello = function (name) {
+  return "Hello, " + name;
+}
+const greeting = returnSayHello("John");
+console.log(greeting);
